4646e2b5d2798a431f4aa76331e6de63f3c84f0e
Steve Chiarelli <spchiare@yahoo.com>
2022-08-08 18:49:30 -0400

    database correction

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, 
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        },
-    );
-    
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
 };
 
